1fb9088aeaeb536eafd9a3438b7fa5de35cd229d
kordian ptak <kordianbird@Gmail.com>
2021-08-25 15:59:14 +0000

    labeled all flash messages in login(), deleted username flash message and inserted password flash message

--- run.py
+++ run.py
@@ -53,18 +53,18 @@ def login():
     if request.method == "POST":
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
-        
+
         if existing_user:
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(request.form.get("username")), "welcome_flash")
             else:
-                flash("Incorrect Username or Password")
+                flash("Incorrect Username or Password", "incorrect_flash")
                 return redirect(url_for("login"))
 
         else:
-            flash("Incorrect Username or Password")
+            flash("Incorrect Username or Password", "incorrect_flash")
             return redirect(url_for("login"))
     return render_template("login.html")
 
