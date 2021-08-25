261da28231ebd8c6ea96c67c02427f5f6c38c932
kordian ptak <kordianbird@Gmail.com>
2021-08-25 15:33:49 +0000

    added login url into href in base.html, created login function in run.py

--- run.py
+++ run.py
@@ -47,6 +47,11 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
 # update to debug=False prior to deployment!
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
