1f401924585e8560176515cc0f64ba0e77e6dd86
kordian ptak <kordianbird@Gmail.com>
2021-09-05 18:46:36 +0000

    added function which expands a recipe when it is clicked

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,11 @@ $(document).ready(function(){
     $('input#input_text, textarea#textarea2').characterCounter();
     $('select').formSelect();
   });
+
+
+$(".btn-floating").click(function(){
+  $(".col").removeClass("l8");
+  $(".card").removeClass("expand");
+  $(this).parents(".col").addClass("l8");
+  $(this).parents(".card").addClass("expand");
+});
