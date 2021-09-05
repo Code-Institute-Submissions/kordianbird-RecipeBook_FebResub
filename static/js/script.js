9cab101ea39bc167a1b73521dad79540fbed98dc
kordian ptak <kordianbird@Gmail.com>
2021-09-05 20:04:45 +0000

    expanding a recipe now hides other recipes

--- static/js/script.js
+++ static/js/script.js
@@ -6,8 +6,9 @@ $(document).ready(function(){
 
 
 $(".btn-floating").click(function(){
-  $(".col").removeClass("l8");
+  $(".col").removeClass("l8 offset-l2");
   $(".card").removeClass("expand");
-  $(this).parents(".col").addClass("l8");
+  $(this).parents(".col").addClass("l8 offset-l2");
   $(this).parents(".card").addClass("expand");
+  $(".col").not($(this).parents(".col")).addClass("Hide");
 });
